1df7074ed8e63f1f42065e8b04057cb0dbb529b2
vickanS <149102936+vickanS@users.noreply.github.com>
2024-10-22 14:17:16 +0200

    Added some animation using framer motion

--- src/components/LoadingScreen/LoadingScreen.jsx
+++ src/components/LoadingScreen/LoadingScreen.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import '../../Styles/LoadingScreen.css';
 import { motion } from 'framer-motion';
+import { useNavigate } from 'react-router-dom';
 
 const LoadingScreen = () => {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate('/set-timer')
+  };
+
     return (
-      <div className="loading-screen">
-        <h1>Interval App</h1>
+      <motion.div 
+        className="loading-screen"
+        style={{ backgroundColor: 'black', minHeight: '100vh'}}
+        initial={{opacity: 0}}
+        animate={{opacity: 1}}
+        transition={{duration: 0.5}}
+      >
+
+        <motion.h1
+          onClick={handleLogoClick}
+          initial={{scale: 1}}
+          whileTap={{scale: 1.1}}
+        >
+          Interval App
+        </motion.h1>
         <p>For all your timing needs</p>
-      </div>
+      </motion.div>
     );
   }  
 
