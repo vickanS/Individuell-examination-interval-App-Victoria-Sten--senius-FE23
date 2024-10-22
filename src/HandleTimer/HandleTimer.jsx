038b367c225d8af1f04bd15fb94639c5498a2fa8
vickanS <149102936+vickanS@users.noreply.github.com>
2024-10-22 14:24:47 +0200

    changes in the code

--- src/HandleTimer/HandleTimer.jsx
+++ src/HandleTimer/HandleTimer.jsx
@@ -1,13 +1,13 @@
-import React, { Children, createContext, useContext, useState } from "react";
+import React, { children, createContext, useContext, useState } from "react";
 
 const TimerContext = createContext();
 
-export const TimerProvider = ({ Children }) => {
+export const TimerProvider = ({ children }) => {
     const [time, setTime] = useState(0);
 
 return (
     <TimerContext.Provider value={{time, setTime}}>
-        { Children }
+        {children}
     </TimerContext.Provider> 
  )
 }
