489425cee8cd80c366438281d65290e157b1fa08
vickanS <149102936+vickanS@users.noreply.github.com>
2024-10-22 14:06:13 +0200

    incorporated TimerProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { TimerProvider, useTimer } from './HandleTimer/HandleTimer'
 import LoadingScreen from './components/LoadingScreen/LoadingScreen'
 import SetTimer from './components/SetTimer/SetTimer'
 import AnalogTimer from './components/AnalogTimer/AnalogTimer'
@@ -11,49 +12,51 @@ import './index.css'
 function App() {
 
   return (
-    <Router>
-      <div >
-        <Routes>
-          <Route path='/' element={<LoadingScreen />} />
-          <Route 
-            path='set-timer' 
-            element={
-              <>
-                <SetTimer />
-                <Menu />
-              </>
-            } 
-          /> 
-          <Route 
-            path='analog-timer' 
-            element={
-              <>
-                <AnalogTimer />
-                <Menu />
-              </>
-            } 
-          /> 
-          <Route 
-            path='digital-timer'
-            element={
-              <>
-                <DigitalTimer />
-                <Menu />
-              </>
-            } 
-          /> 
-          <Route 
-            path='Alarm-view'
-            element={
-              <>
-                <AlarmView />
-                <Menu />
-              </>
-            } 
-          /> 
-        </Routes>
-      </div>
-    </Router>
+    <TimerProvider>
+      <Router>
+        <div >
+          <Routes>
+            <Route path='/' element={<LoadingScreen />} />
+            <Route 
+              path='set-timer' 
+              element={
+                <>
+                  <SetTimer />
+                  <Menu />
+                </>
+              } 
+            /> 
+            <Route 
+              path='analog-timer' 
+              element={
+                <>
+                  <AnalogTimer />
+                  <Menu />
+                </>
+              } 
+            /> 
+            <Route 
+              path='digital-timer'
+              element={
+                <>
+                  <DigitalTimer />
+                  <Menu />
+                </>
+              } 
+            /> 
+            <Route 
+              path='Alarm-view'
+              element={
+                <>
+                  <AlarmView />
+                  <Menu />
+                </>
+              } 
+            /> 
+          </Routes>
+        </div>
+      </Router>
+    </TimerProvider>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { TimerProvider } from './HandleTimer/HandleTimer.jsx'
 import './index.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <TimerProvider>
     <App />
+    </TimerProvider>
   </StrictMode>,
 )
