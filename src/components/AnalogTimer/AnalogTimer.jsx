8729d3882428d0cfb4d2d15208246d964361da75
vickanS <149102936+vickanS@users.noreply.github.com>
2024-10-23 17:33:53 +0200

    Implemented analog timer functionality and styling: Added timer logic with rotating hands in AnalogTimer.jsx, added styles in AnalogTimer.css

--- src/components/AnalogTimer/AnalogTimer.jsx
+++ src/components/AnalogTimer/AnalogTimer.jsx
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
+import { useTimer } from '../../HandleTimer/HandleTimer'; 
+import EasyTimer from 'easytimer.js';
 import '../../Styles/AnalogTimer.css';
 
+
 const AnalogTimer = () => {
+    const { time } = useTimer();
+    const timerRef = useRef(null);
+    const secondsRef = useRef(null);
+    const minutesRef = useRef(null);
+
+
+    useEffect(() => {
+        if (!timerRef.current) {
+            timerRef.current = new EasyTimer();
+        }
+
+        if (time) {
+            timerRef.current.start({ countdown: true, startValues: { seconds: time * 60 } });
+        }
+
+        const interval = setInterval(() => {
+            const totalSeconds = timerRef.current.getTimeValues().seconds + 
+                                (timerRef.current.getTimeValues().minutes * 60);
+            const seconds = totalSeconds % 60;
+            const minutes = Math.floor(totalSeconds / 60);
+
+            if (secondsRef.current && minutesRef.current) {
+                secondsRef.current.style.transform = `rotate(${(seconds / 60) * 360}deg)`;
+                minutesRef.current.style.transform = `rotate(${(minutes / 60) * 360}deg)`;
+            }
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+            timerRef.current.stop();
+        };
+    }, [time]);
+
     return (
-        <div className='Analog'>
-            
+        <div className="analog-timer">
+            <h2>Analog Timer</h2>
+            <div className='analogtimer'>
+                <div className='clock'>
+                    <div className='hand minute-hand' ref={minutesRef}></div>
+                    <div className='hand second-hand' ref={secondsRef}></div>
+                </div>
+            </div>
         </div>
-    )
-} 
+    );
+};
 
 
 export default AnalogTimer;
