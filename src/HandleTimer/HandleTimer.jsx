30f227d226cd85542143eaa5f5bfd795fbc61ff3
vickanS <149102936+vickanS@users.noreply.github.com>
2024-10-24 14:17:00 +0200

    Added timer context and provider for global state management

--- src/HandleTimer/HandleTimer.jsx
+++ src/HandleTimer/HandleTimer.jsx
@@ -1,4 +1,4 @@
-import React, { children, createContext, useContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 const TimerContext = createContext();
 
