378036956f1de55b2b4c19a98808feb98f24e375
vickanS <149102936+vickanS@users.noreply.github.com>
2024-10-25 16:46:03 +0200

    added logic for the digital timer

--- src/components/DigitalTimer/DigitalTimer.jsx
+++ src/components/DigitalTimer/DigitalTimer.jsx
@@ -1,47 +1,31 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useTimer } from '../../HandleTimer/HandleTimer';
 import '../../Styles/DigitalTimer.css';
 import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
 
 const DigitalTimer = () => {
-    const { time, setTime } = useTimer();
+    const { time, handleAbort } = useTimer();
     const navigate = useNavigate();
 
-    useEffect(() => {
-        if (time <= 0) {
-            return;
-        }
+    const minutes = Math.floor(time / 60); 
+    const seconds = time % 60; 
 
-        const intervalId = setInterval(() => {
-            setTime(prevTime => {
-                if (prevTime > 0) {
-                    return prevTime - 1; 
-                } else {
-                    clearInterval(intervalId); 
-                    return 0; 
-                }
-            });
-        }, 1000);
+    const formattedMinutes = ('0' + minutes).slice(-2); 
+    const formattedSeconds = ('0' + seconds).slice(-2);
 
-        return () => clearInterval(intervalId); 
-    }, [time, setTime]);
-
-    const handleAbort = () => {
-        setTime(0); 
+    const abortTimer = () => {
+        handleAbort();
         navigate('/set-timer'); 
     };
-
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
 
     return (
         <div className="digital-timer">
             <h2>Digital Timer</h2>
             <div className="timer-display">
-                <h1>{minutes}:{('0' + seconds).slice(-2)}</h1>
+                <h1>{formattedMinutes}:{formattedSeconds}</h1> 
             </div>
-            <motion.button onClick={handleAbort}>Abort Timer</motion.button>
+            <motion.button onClick={abortTimer}>Abort Timer</motion.button>
         </div>
     );
 };
