9dd1192c46a2c21bb35513427e15d393d6216657
vickanS <149102936+vickanS@users.noreply.github.com>
2024-10-25 18:22:05 +0200

    Added content in the AlarmView component

--- src/components/AlarmView/AlarmView.jsx
+++ src/components/AlarmView/AlarmView.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../../Styles/AlarmView.css';
+import alarmicon from '../../assets/alarmicon.svg'; 
 
 
 const AlarmView = () => {
+   const navigate = useNavigate();
+
+   const handleGoToSetTimer = () => {
+    navigate('/set-timer')
+   }
+ 
+
     return (
-        <div className='Alarm-view'>
-            
+        <div className='alarm-view'>
+            <img src={alarmicon} alt='Alarm icon' className='alarm-icon' />
+            <h2>Time is up!</h2>
+            <button onClick={handleGoToSetTimer}>Set new timer!</button>
         </div>
     )
-} 
-
-
-
-
-
-
+}
 
 
 export default AlarmView;
