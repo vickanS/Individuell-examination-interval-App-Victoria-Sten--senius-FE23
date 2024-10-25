118e09da00621bb35bc71c4cf787790e48a7a89c
vickanS <149102936+vickanS@users.noreply.github.com>
2024-10-25 16:48:25 +0200

    Updated startTimer logic in HandleTimer to start timer in minutes and handle abort functionality so that the timers will sync

--- src/HandleTimer/HandleTimer.jsx
+++ src/HandleTimer/HandleTimer.jsx
@@ -1,17 +1,47 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const TimerContext = createContext();
 
 export const TimerProvider = ({ children }) => {
     const [time, setTime] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
 
-return (
-    <TimerContext.Provider value={{time, setTime}}>
-        {children}
-    </TimerContext.Provider> 
- )
-}
+    useEffect(() => {
+        let intervalId;
+        if (isRunning && time > 0) {
+            intervalId = setInterval(() => {
+                setTime(prevTime => {
+                    if (prevTime > 0) {
+                        return prevTime - 1; 
+                    } else {
+                        clearInterval(intervalId);
+                        setIsRunning(false);
+                        return 0;
+                    }
+                });
+            }, 1000);
+        }
+
+        return () => clearInterval(intervalId); 
+    }, [isRunning, time]);
+
+    const startTimer = (initialTime) => {
+        setTime(initialTime * 60);
+        setIsRunning(true);
+    };
+
+    const handleAbort = () => {
+        setIsRunning(false);
+        setTime(0);
+    };
+
+    return (
+        <TimerContext.Provider value={{ time, setTime, isRunning, startTimer, handleAbort }}>
+            {children}
+        </TimerContext.Provider>
+    );
+};
 
 export const useTimer = () => {
-    return useContext(TimerContext)
-}
+    return useContext(TimerContext);
+};
